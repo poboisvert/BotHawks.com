502196d5b6d521100788ae5be1391c6e1e55bc14
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-09-13 18:32:32 -0400

    init

--- client/src/common/Block/index.js
+++ client/src/common/Block/index.js
@@ -4,6 +4,7 @@ import logo_nigh from '../logo_night.png';
 
 import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+
 // BrowserRouter is the router implementation for HTML5 browsers (vs Native).
 // Link is your replacement for anchor tags.
 // Route is the conditionally shown component based on matching a path to a URL.
