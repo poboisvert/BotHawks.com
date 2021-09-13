194ab422cd0311c6534d4f67643f44470dfe0818
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-09-13 15:50:57 -0400

    Logo revised

--- client/src/common/Block/index.js
+++ client/src/common/Block/index.js
@@ -1,16 +1,33 @@
 import * as S from './styles';
-import Logo from '../logo.png';
+import logo from '../logo.png';
+import logo_nigh from '../logo_night.png';
+
 import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 // BrowserRouter is the router implementation for HTML5 browsers (vs Native).
 // Link is your replacement for anchor tags.
 // Route is the conditionally shown component based on matching a path to a URL.
 // Switch returns only the first matching route rather than all matching routes.
 
 const Block = ({ content }) => {
+  const [logo1, setLogo1] = useState('');
+  const [today, setDate] = useState(new Date()); // Save the current date to be able to trigger an update
+
+  useEffect(() => {
+    let result = 0;
+    const logos = [logo, logo_nigh];
+
+    const hour = today.getHours();
+    if (hour > 17) {
+      setLogo1((result = logos[1]));
+    } else {
+      setLogo1((result = logos[0]));
+    }
+  }, [content]);
   return (
     <S.Block id='mission'>
       <S.ContentWrapper>
-        <S.Logo src={Logo} />
+        <S.Logo src={logo1} />
         <S.Content>
           {content} <br />
           <Link
